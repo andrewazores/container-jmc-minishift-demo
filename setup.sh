772634ee97438642929c33051e99e2cb87f28e47
Andrew Azores <me@andrewazor.es>
2019-03-11 13:43:02 -0400

    Correct probe port

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 oc new-app --name jmx-listener docker.io/andrewazores/container-jmx-docker-listener
-oc set probe --readiness --liveness --open-tcp=9091 dc/jmx-listener
+oc set probe --readiness --liveness --open-tcp=9090 dc/jmx-listener
