fc112efb3753a07a536711e59aa17ab7e7c5ad9b
Andrew Azores <me@andrewazor.es>
2019-04-25 13:55:34 -0400

    Add/modify scripts for running client in daemon mode

--- run-daemon.sh
+++ run-daemon.sh
@@ -5,25 +5,16 @@ set -e
 
 LABEL="name=jmxclient"
 
-function cleanup() {
-    oc delete --ignore-not-found pods,services,routes -l "$LABEL" > /dev/null 2>&1
-}
-
 function oc_expose() {
     sleep 10 # TODO replace this with a proper wait for the pod to be available
     oc expose -l "$LABEL" svc/jmx-client
 }
 
-trap cleanup EXIT
-cleanup
 oc_expose > /dev/null 2>&1 &
 
 oc run \
     --labels="$LABEL" \
     --restart=Never \
-    --rm \
-    --wait \
-    --attach --stdin --tty \
     --port=8080 --expose \
     --image=docker.io/andrewazores/container-jmx-client:latest --image-pull-policy=Always \
-    -- jmx-client "$@"
+    -- jmx-client -d
